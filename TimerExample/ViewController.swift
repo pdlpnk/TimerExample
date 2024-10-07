6452f1eb5debd5356dbf71ed27f1095f19475dd6
Juicefai <rimchik3010@gmail.com>
2024-10-07 17:04:52 +0300

    Initial Commit

---
+++ TimerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerExample
+//
+//  Created by Рома Подлипняк on 07.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TimerExampleTests/TimerExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerExampleTests.swift
+//  TimerExampleTests
+//
+//  Created by Рома Подлипняк on 07.10.2024.
+//
+
+import Testing
+@testable import TimerExample
+
+struct TimerExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
